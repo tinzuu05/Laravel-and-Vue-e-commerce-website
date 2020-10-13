119b699b5536356350f255b15b862f783af63904
tinzuu05 <64735248+tinzuu05@users.noreply.github.com>
2020-10-13 16:52:54 +0800

    10.13

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -52,8 +52,8 @@
 
 @section('js')
 {{-- summerNote --}}
-{{-- <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script> --}}
-{{-- <script>
+{{-- <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+<script>
     $(document).ready(function() {
         $('#summernote').summernote({
 
